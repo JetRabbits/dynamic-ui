a955331ee3daeb01d7a6aa08f1e01c79d59ec262
mormih <mormih@gmail.com>
2021-06-24 06:21:35 +0400

    fix parser save widgets for stories

--- lib/utils/parameters_utils.dart
+++ lib/utils/parameters_utils.dart
@@ -1,6 +1,8 @@
 String substituteParams(String template, Map<String, dynamic> parameters) {
   String result = template;
   parameters.keys
-      .forEach((key) => result = result.replaceAll("#$key#", parameters[key]));
+      .forEach((key) {
+        result = parameters[key] is String ? result.replaceAll("#$key#", parameters[key]): result;
+      });
   return result;
 }
